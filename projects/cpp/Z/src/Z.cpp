e846bdac6b3f4e9bd51eeab7627706d1a2bb048b
Ahmad <2a7mad@gmail.com>
2010-12-31 11:50:18 -0500

    parsing: 1 digit, 4 ops

--- projects/cpp/Z/src/Z.cpp
+++ projects/cpp/Z/src/Z.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <map>
+#include <stack>
 
 using namespace std;
 
+map<char, int> operators;
+
+bool is_operator(char c) {
+	return operators.find(c) != operators.end();
+}
+
+int precedence(char c) {
+	return operators[c];
+}
+
+bool is_opening_paren(char c) {
+	return c == '(';
+}
+
+bool is_closing_paren(char c) {
+	return c == ')';
+}
+
 int main() {
+
+	operators['+'] = 10;
+	operators['-'] = 10;
+	operators['*'] = 20;
+	operators['/'] = 20;
 
 	while (true) {
 
@@ -11,7 +36,49 @@ int main() {
 		string line;
 		cin >> line;
 
-		cout << "[1] " << line << endl;
+		cout << "line: \"" << line << "\"" << endl;
+
+		stack<char> stack;
+		string postfix;
+
+		for (unsigned int i = 0; i < line.length(); i++) {
+			char c = line[i];
+
+			if (isdigit(c)) {
+				postfix += line[i];
+
+			} else if (is_operator(c)) {
+
+				while (!stack.empty() && !is_opening_paren(stack.top())
+						&& precedence(stack.top()) >= precedence(c)) {
+					postfix += stack.top();
+					stack.pop();
+				}
+
+				stack.push(c);
+
+			} else if (is_opening_paren(c)) {
+				stack.push(c);
+
+			} else if (is_closing_paren(c)) {
+
+				while (!is_opening_paren(stack.top())) {
+					postfix += stack.top();
+					stack.pop();
+				}
+				stack.pop();
+
+			}
+
+		}
+
+		while (!stack.empty()) {
+			postfix += stack.top();
+			stack.pop();
+		}
+
+		cout << "postfix: \"" << postfix << "\"" << endl;
+
 	}
 
 	return 0;
