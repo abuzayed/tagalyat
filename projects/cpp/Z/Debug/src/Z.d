cc7664f5d99bca30615461b0a24866fa2946ce76
Ahmad Abu-Zayedeh <ahmad@ahmad.home>
2010-12-30 20:18:50 -0500

    new project

---
+++ projects/cpp/Z/Debug/src/Z.d
@@ -0,0 +1,1 @@
+src/Z.d src/Z.o: ../src/Z.cpp
---
+++ projects/cpp/Z/src/Z.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Z.cpp
+// Author      : Ahmad Abu-Zayedeh
+// Version     :
+// Copyright   : @ 2011
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Zzzzzz" << endl; // prints Zzzzzz
+	return 0;
+}
