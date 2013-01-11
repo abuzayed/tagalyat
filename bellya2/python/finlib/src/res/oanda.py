df8e1fb1fd3655ed73574b9763a9bb2c17ee1b6a
ahmad <2a7mad@gmail.com>
2013-01-11 13:23:28 -0500

    plot

--- bellya2/python/finlib/src/res/oanda.py
+++ bellya2/python/finlib/src/res/oanda.py
@@ -1,25 +1,41 @@
 import time
+from time import mktime
+
+from datetime import datetime
+from datetime import timedelta
+
 import sys
 from collections import deque
+
+import matplotlib.pyplot as plt
+
 
 class Bar:
     def __init__(self, period, handler):
         self.period = period
         self.handler = handler
-        self.current_bar = 0
+        
+        self.bar_end = datetime.fromtimestamp(0)
         self.last_ask = 0
 
     def handleTick(self, tick):
-        ts = tick[0]
-        secs = ((ts.tm_hour * 60 + ts.tm_min) * 60) + ts.tm_sec
-        this_bar = secs / self.period
 
-        if this_bar != self.current_bar:
-            self.current_bar = this_bar
+        if tick[0] >= self.bar_end:
             if self.last_ask > 0:
-                bar = (self.last_ask)
+                bar = (self.bar_start, self.last_ask)
                 self.handler.handleBar(bar)
-        
+
+            ts = tick[0]
+            secs = ((ts.hour * 60 + ts.minute) * 60) + ts.second
+            this_bar = secs / self.period
+            this_bar = this_bar * self.period
+            this_bar, bar_secs = divmod(this_bar, 60)    
+            bar_hrs, bar_mins = divmod(this_bar, 60) 
+               
+            self.bar_start = datetime(ts.year, ts.month, ts.day, bar_hrs, bar_mins, bar_secs)
+            
+            self.bar_end = self.bar_start + timedelta(seconds=self.period)
+
         self.last_ask = tick[2]
 
 
@@ -48,9 +64,14 @@ class Test:
         self.buy = False
         self.sell = False
         self.pnl = 0
+        
+        self.dates = []
+        self.asks = []
+        self.sma1list = []
+        self.sma2list = []
+        self.sma3list = []
 
     def handleTick(self, tick):
-        
         if self.buy:
             print 'buying at: ' + str(tick[2])
             self.pnl = self.pnl - tick[2]
@@ -68,11 +89,17 @@ class Test:
         self.spread = tick[2] - tick[1]
         self.bar.handleTick(tick)
 
-    def handleBar(self, bar):
-        self.sma1.add(bar)
-        self.sma2.add(bar)
-        self.sma3.add(bar)
-#        print 'Test.handleBar: bar=' + str(bar) + ', sma1=' + str(self.sma1.value) + ', sma2=' + str(self.sma2.value) + ', sma3=' + str(self.sma3.value)
+    def handleBar(self, bar):        
+        self.sma1.add(bar[1])
+        self.sma2.add(bar[1])
+        self.sma3.add(bar[1])
+
+        self.dates.append(bar[0])
+        self.asks.append(bar[1] / 100000000.0)
+        self.sma1list.append(self.sma1.value / 100000000.0)
+        self.sma2list.append(self.sma2.value / 100000000.0)
+        self.sma3list.append(self.sma3.value / 100000000.0)
+        
 
         if self.pos == 0:
             if self.sma1.value > self.sma2.value and self.spread <= 9000:
@@ -80,10 +107,24 @@ class Test:
         else:
             if self.sma1.value < self.sma3.value or self.spread > 11000:
                 self.sell = True
-            
+                
+                
+    def handleEOD(self):
+        self.plot()
+                
+
+    def plot(self):
+    
+        plt.plot(self.dates, self.asks)
+        plt.plot(self.dates, self.sma1list)
+        plt.plot(self.dates, self.sma2list)
+        plt.plot(self.dates, self.sma3list)
+
+        plt.show()
+        
 
 def parseDatetime(v):
-    return time.strptime(v, "%d/%m/%y %H:%M:%S")
+    return  datetime.fromtimestamp(mktime(time.strptime(v, "%d/%m/%y %H:%M:%S")))
 
 def parsePrice(v):
     d = v.index('.')
@@ -96,7 +137,7 @@ def parseTick(line):
     return (timestamp, bid, ask)    
 
 def main():
-    mdfilename = 'C:\\ahmad\\data\\EC0299F7A8DE9926DAF7F2A6B2093F43.txt'
+    mdfilename = 'C:\\ahmad\\data\\20100317.txt'
     mdfile = open(mdfilename, 'r')
 
     test = Test()
@@ -109,10 +150,11 @@ def main():
             tick = parseTick(line)
             test.handleTick(tick)
             
-#            if count == 100:
+#            if count == 200:
 #                break
         else:
             break
+    test.handleEOD()
 
 if __name__ == '__main__':
     main()
